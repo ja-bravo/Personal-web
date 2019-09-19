dacfd39a817be0c7931c7d0ff11c3ed958f05a95
Jose Bravo <ja.bravo.isidro@gmail.com>
2019-09-19 10:09:41 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
 
 class BlogIndex extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    // const posts = data.allMarkdownRemark.edges;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
-        <Bio />
+        <h1>Work in Progress</h1>
+        {/* <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
@@ -41,13 +42,13 @@ class BlogIndex extends React.Component {
               </section>
             </article>
           )
-        })}
+        })} */}
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
@@ -72,4 +73,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
